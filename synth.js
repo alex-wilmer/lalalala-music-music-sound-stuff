06e228a7ace26085ee55c8a0b886d724d907ade1
Alex Wilmer <djazium@gmail.com>
2019-10-10 19:25:05 -0400

    audiokeys

--- synth.js
+++ synth.js
@@ -1,1 +1,23 @@
-console.log("hey this should really work");
+import AudioKeys from "audiokeys";
+
+let ctx = new AudioContext();
+
+// create a keyboard
+var keyboard = new AudioKeys();
+
+let oscMap = {};
+
+keyboard.down(function(note) {
+  let osc = ctx.createOscillator();
+  osc.frequency.value = note.frequency;
+  osc.connect(ctx.destination);
+  osc.start();
+
+  oscMap[note.frequency] = osc;
+});
+
+keyboard.up(function(note) {
+  let osc = oscMap[note.frequency];
+  osc.stop();
+  // do things with the note object
+});
